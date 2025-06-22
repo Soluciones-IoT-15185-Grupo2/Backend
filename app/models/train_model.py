2c6dd0494d516e3fcb0c0c94827c7aa72de780d2
Ank222001 <u20181h103@upc.edu.pe>
2025-06-21 21:01:45 -0500

    Create train_model.py
    feat: add structure

---
+++ app/models/train_model.py
@@ -0,0 +1,8 @@
+# train_model.py
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import os
