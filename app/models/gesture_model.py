d20c1a5ace2b7288dc746cefecc6a792a81d8a72
Ank222001 <102317309+Ank222001@users.noreply.github.com>
2025-07-08 14:39:31 -0500

    Update gesture_model.py

--- app/models/gesture_model.py
+++ app/models/gesture_model.py
@@ -8,9 +8,9 @@ model = tf.keras.models.load_model("app/models/gesture_model.h5")
 classes = np.load("app/models/classes.npy")
 
 def predict_gesture(sensor_array: list) -> str:
-    """Recibe una lista de 90 valores (ax, ay, az, gx, gy, gz) de ambos guantes"""
-    if len(sensor_array) != 90:
-        raise ValueError("Se esperaban 90 valores de entrada")
+    """Recibe una lista de 60 valores (ax, ay, az, gx, gy, gz) de ambos guantes"""
+    if len(sensor_array) != 60:
+        raise ValueError("Se esperaban 60 valores de entrada")
     X = np.array(sensor_array).reshape(1, -1)
     X = X / np.linalg.norm(X, axis=1, keepdims=True)
     prediction = model.predict(X)
