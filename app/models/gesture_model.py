3fb143c1a8963bc529b72601df1becb87eb79067
Ank222001 <102317309+Ank222001@users.noreply.github.com>
2025-07-08 12:58:47 -0500

    feat: update gesture_models

--- app/models/gesture_model.py
+++ app/models/gesture_model.py
@@ -1,13 +1,18 @@
+# app/models/gesture_model.py
+
 import numpy as np
 import tensorflow as tf
-from app.core.config import MODEL_PATH
+import os
 
-model = tf.keras.models.load_model(MODEL_PATH)
-label_map = ['a', 'b', 'c', 'd', 'e']  # Reemplaza con tus clases reales
+model = tf.keras.models.load_model("app/models/gesture_model.h5")
+classes = np.load("app/models/classes.npy")
 
-def predict_gesture(data):
-    input_array = np.array([[data.ax, data.ay, data.az, data.gx, data.gy, data.gz]])
-    input_array = input_array / np.linalg.norm(input_array)
-    prediction = model.predict(input_array)
-    predicted_index = np.argmax(prediction)
-    return label_map[predicted_index]
+def predict_gesture(sensor_array: list) -> str:
+    """Recibe una lista de 90 valores (ax, ay, az, gx, gy, gz) de ambos guantes"""
+    if len(sensor_array) != 90:
+        raise ValueError("Se esperaban 90 valores de entrada")
+    X = np.array(sensor_array).reshape(1, -1)
+    X = X / np.linalg.norm(X, axis=1, keepdims=True)
+    prediction = model.predict(X)
+    predicted_label = classes[np.argmax(prediction)]
+    return predicted_label
