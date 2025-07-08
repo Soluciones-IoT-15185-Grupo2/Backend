6135eba8497de01f69e5b186d74f185394f454a2
acrispin06 <alvarocrispin0604@gmail.com>
2025-07-08 17:17:08 -0500

    feat: adding CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 # app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from app.models.gesture_model import predict_gesture
 
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes. En producción, especifica dominios específicos
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP
+    allow_headers=["*"],  # Permite todos los headers
+)
 
 class SensorData(BaseModel):
     data: list  # Lista de 90 valores
